55de03bc993e813190f7da9b301e8a0a0f5fc193
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-21 17:39:28 +0300

    Added OOP SOLID Open Closed Principle Mistake.

--- OOPBestPractices/OOP/SOLID/Open Closed Principle/2) Mistake/MonitorAssembly/Monitor.cs
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/2) Mistake/MonitorAssembly/Monitor.cs
@@ -1,15 +1,35 @@
-﻿namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly;
+﻿global using OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly.Internal;
+
+namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly;
 public static class Monitor {
-    public static void Log(string log) {
+    public static void Log(LogType type, string log) {
         // Simulating logging
         // as common domain logic
+
+        // Hardcoded logger types enumeration
         try {
-            Console.Write(log);
+            if (type == LogType.None) {
+                return;
+            }
+
+            if (type == LogType.Console) {
+                Console.Write(log);
+            }
+
+            if (type == LogType.File) {
+                FileLogger.Log(log);
+            }
         }
         catch (Exception ex) {
-            using StreamWriter writer = new("error.log");
-            writer.WriteLine($"{ex.Message}");
             throw new Exception("Logging error", ex);
+        }
+    }
+
+    // Different but required logic
+    // of represented logger
+    public static void Close(LogType type) {
+        if (type == LogType.File) {
+            FileLogger.Close();
         }
     }
 }
