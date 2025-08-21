199059216010c883988d87335edff1f92d814f41
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-21 18:33:13 +0300

    Initialized OOP SOLID Open Closed Principle Refactoring.

---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/4.) Refactoring/MonitorAssembly/Monitor.cs
@@ -0,0 +1,35 @@
+﻿global using OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly.Internal;
+
+namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly;
+public static class Monitor {
+    public static void Log(LogType type, string log) {
+        // Simulating logging
+        // as common domain logic
+
+        // Hardcoded logger types enumeration
+        try {
+            if (type == LogType.None) {
+                return;
+            }
+
+            if (type == LogType.Console) {
+                Console.Write(log);
+            }
+
+            if (type == LogType.File) {
+                FileLogger.Log(log);
+            }
+        }
+        catch (Exception ex) {
+            throw new Exception("Logging error", ex);
+        }
+    }
+
+    // Different but required logic
+    // of represented logger
+    public static void Close(LogType type) {
+        if (type == LogType.File) {
+            FileLogger.Close();
+        }
+    }
+}
