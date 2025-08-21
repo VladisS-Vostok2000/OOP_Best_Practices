868bd0820ba6df62a8f14f7a60306cc84586a62d
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-21 19:19:40 +0300

    Added OOP SOLID Open Closed Principle Refactoring.

---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/4.) Refactoring/LoggingAbstractionsAssembly/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.LoggingAssembly.Abstractions;
+// Defining common interface
+// in separately assembly
+// so we never need now
+// to recompile injection
+// consumers anymore
+public interface ILogger {
+    public void Log(string log);
+}
