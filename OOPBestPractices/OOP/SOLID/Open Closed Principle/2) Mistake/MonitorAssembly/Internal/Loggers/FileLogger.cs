9e93a284b39c39dee985b61bfcb8d8a3d2b75f62
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-21 17:39:28 +0300

    Added OOP SOLID Open Closed Principle Mistake.

---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/2) Mistake/MonitorAssembly/Internal/Loggers/FileLogger.cs
@@ -0,0 +1,22 @@
+﻿namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly.Internal;
+
+// Adding new logger
+internal static class FileLogger {
+    private static StreamWriter Writer { get; set; }
+
+
+
+    static FileLogger() {
+        Writer = new("log.txt");
+    }
+
+
+
+    public static void Log(string log) {
+        Writer.WriteLine(log);
+    }
+
+    public static void Close() {
+        Writer.Dispose();
+    }
+}
