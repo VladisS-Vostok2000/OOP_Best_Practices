16b7d94b219dba1f124ec6de58ff7bc0936308a2
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-21 18:29:59 +0300

    Added OOP SOLID Open Closed Principle Escalation.

---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/3) Escalation/MonitorAssembly/Loggers/MonitorLogger.cs
@@ -0,0 +1,27 @@
+﻿namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly;
+
+// Implementing new logger as usual
+// Crap, now logger must be public
+// for domain logic injection
+// even if its not being use
+// So we replace `MonitorLogger`
+// from `Internal` to `Loggers`
+public class MonitorLogger {
+    private string IP { get; init; }
+
+
+
+    public MonitorLogger(string ip) {
+        IP = ip;
+    }
+
+
+
+    public void Log(string log) {
+        // Some log domain logic here
+    }
+
+    public void Close() {
+        // Some log domain logic here
+    }
+}
