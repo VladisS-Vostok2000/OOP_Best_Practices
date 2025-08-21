b376faa94ab5c2994a3c62cf29cde0cf9d233241
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-21 17:23:00 +0300

    Added OOP SOLID Open Closed Principle Initial.

---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/1) Initial/MonitorAssembly/Monitor.cs
@@ -0,0 +1,15 @@
+﻿namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly;
+public static class Monitor {
+    public static void Log(string log) {
+        // Simulating logging
+        // as common domain logic
+        try {
+            Console.Write(log);
+        }
+        catch (Exception ex) {
+            using StreamWriter writer = new("error.log");
+            writer.WriteLine($"{ex.Message}");
+            throw new Exception("Logging error", ex);
+        }
+    }
+}
