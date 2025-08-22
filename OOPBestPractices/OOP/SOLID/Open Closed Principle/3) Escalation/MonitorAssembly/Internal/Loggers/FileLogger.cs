7e49270d77626219bde372b5bce443388e338697
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-22 16:51:57 +0300

    Initialized OOP SOLID OpenClosedPrinciple Escalation.

---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/3) Escalation/MonitorAssembly/Internal/Loggers/FileLogger.cs
@@ -0,0 +1,21 @@
+﻿namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly.Internal;
+
+internal static class FileLogger {
+    private static StreamWriter Writer { get; set; }
+
+
+
+    static FileLogger() {
+        Writer = new("log.txt");
+    }
+
+
+
+    public static void Log(string log) {
+        Writer.WriteLine(log);
+    }
+
+    public static void Close() {
+        Writer.Dispose();
+    }
+}
---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/3) Escalation/MonitorAssembly/Monitor.cs
@@ -0,0 +1,31 @@
+﻿global using OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly.Internal;
+
+namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.MonitorAssembly;
+public static class Monitor {
+    public static void Log(LogType type, string log) {
+        // Simulating logging
+        // as common domain logic
+        try {
+            if (type == LogType.None) {
+                return;
+            }
+
+            if (type == LogType.Console) {
+                Console.Write(log);
+            }
+
+            if (type == LogType.File) {
+                FileLogger.Log(log);
+            }
+        }
+        catch (Exception ex) {
+            throw new Exception("Logging error", ex);
+        }
+    }
+
+    public static void Close(LogType type) {
+        if (type == LogType.File) {
+            FileLogger.Close();
+        }
+    }
+}
