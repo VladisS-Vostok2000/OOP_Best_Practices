fb5c5000198fa83e241d2e1309c036f164b88202
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-22 12:10:00 +0300

    Initialized OOP SOLID Open Closed Principle Solution.

---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/5.) Solution/LoggingAssembly/FileLogger.cs
@@ -0,0 +1,20 @@
+﻿namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.LoggingAssembly;
+public sealed class FileLogger : ILogger, IDisposable {
+    private StreamWriter Writer { get; set; }
+
+
+
+    FileLogger(string path) {
+        Writer = new(path);
+    }
+
+
+
+    public void Log(string log) {
+        Writer.WriteLine(log);
+    }
+
+    public void Dispose() {
+        Writer.Dispose();
+    }
+}
