32a75c26fb379c14ff87f313e980cbd8adc60c4c
VladisS Pestrikoff <VladisS.vostok2000@gmail.com>
2025-08-22 12:10:22 +0300

    Added OOP SOLID Open Closed Principle Solution.

---
+++ OOPBestPractices/OOP/SOLID/Open Closed Principle/5.) Solution/LoggingAssembly/MonitorLogger.cs
@@ -0,0 +1,32 @@
+﻿namespace OOPBestPractices.OOP.SOLID.OpenClosedPrinciple.LoggingAssembly;
+
+// Implementing logger
+// as usual
+// No more thouthents 
+// of changes
+// and recompilations
+// Expansion? Easy peasy
+// Its full enough now
+public sealed class MonitorLogger : ILogger, IDisposable {
+    private string IP { get; init; }
+
+
+
+    public MonitorLogger(string ip) {
+        IP = ip;
+    }
+
+
+
+    public void Log(string log) {
+        // Some log domain logic here
+    }
+
+    public void Close() {
+        // Some log domain logic here
+    }
+
+    public void Dispose() {
+        // Some application logic here
+    }
+}
